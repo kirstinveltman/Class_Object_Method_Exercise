179a5a7db852ab2ff9399d11762ea8b6881e21a4
Kirstin Veltman <kirstin@veltman.co>
2018-05-24 14:49:23 -0700

    Add project files.

---
+++ Class_Object_Method_Exercise/Math.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Object_Method_Exercise
+{
+    public class Math
+    {
+        public int Number { get; set; }
+
+
+        public int Multiply(int num)
+        {
+            return num * Number;
+        }
+
+        public int Subtract(int num)
+        {
+            return num - Number;
+        }
+
+        public int Add(int num)
+        {
+            return num + Number;
+        }
+
+        public Math()
+        {
+            Number = 0;
+        }
+
+    }
+}
---
+++ Class_Object_Method_Exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Object_Method_Exercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please enter a whole, real number: ");
+            math.Number = Convert.ToInt32(Console.ReadLine());
+
+            int result1 = math.Add(13);
+            Console.WriteLine($"Your number added to 13 is: {result1}");
+
+            int result2 = math.Subtract(13);
+            Console.WriteLine($"Your number subtracted from 13 is: {result2}");
+
+            int result3 = math.Multiply(13);
+            Console.WriteLine($"Your number multiplied by 13 is: {result3}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
